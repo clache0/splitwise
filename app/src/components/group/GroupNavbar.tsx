c7983886d1569c0a3c08fb1054d584ca3abb8c3a
Clayton Cheng <clay578@gmail.com>
2024-04-23 16:38:18 -0700

    fix type warning for GroupNavbar

--- app/src/components/group/GroupNavbar.tsx
+++ app/src/components/group/GroupNavbar.tsx
@@ -1,21 +1,34 @@
 import { useEffect, useState } from 'react';
 import config from '../../../config.json'
 
+type Group = {
+  name: string;
+  members: Member[];
+};
+
+type Member = {
+  _id: string;
+}
+
+type Error = {
+  message: string;
+};
+
 const GroupNavbar = () => {
   const serverUrl = config.serverUrl + '/groups' + '/662306fcc4de419f942fc418';
 
-  const [group, setGroup] = useState(null);
+  const [group, setGroup] = useState<Group | null>(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     fetch(serverUrl)
       .then(response => response.json())
-      .then(data => {
+      .then((data: Group) => {
         setGroup(data);
         setLoading(false);
       })
-      .catch(error => {
+      .catch((error: Error) => {
         console.error('Error fetching data:', error);
         setError(error);
         setLoading(false);
@@ -28,13 +41,7 @@ const GroupNavbar = () => {
   return (
     <>
       <h1>Group Navbar</h1>
-
-      {group ? (
-        <h1>Group Name: {group.name}</h1>
-      ) : (
-        <p>Loading...</p>
-      )
-      }
+      <h1>Group Name: {group?.name}</h1>
     </>
   )
 }
