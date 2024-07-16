85cd85e55734aac4e5dad627a2c9815bda6d3b3d
Clayton Cheng <clay578@gmail.com>
2024-07-16 16:58:23 -0700

    filter expenses by month and year

--- app/src/components/expense/ExpenseList.tsx
+++ app/src/components/expense/ExpenseList.tsx
@@ -1,6 +1,7 @@
 import ExpenseComponent from "./ExpenseComponent";
 import { Expense, Group, User } from "../group/GroupComponent";
 import "../../styles/components/expense/ExpenseList.css"
+import { useState } from "react";
 
 interface ExpenseListProps {
   group: Group | null;
@@ -10,10 +11,43 @@ interface ExpenseListProps {
   users: User[] | null;
 }
 const ExpenseList: React.FC<ExpenseListProps> = ({ group, groupExpenses, onUpdateExpense, onDeleteExpense, users }) => {
-  
-  if (!groupExpenses) return <p>Group expenses null</p>
+  const [selectedMonth, setSelectedMonth] = useState<string | null>(null);
+  const [selectedYear, setSelectedYear] = useState<string | null>(null);
 
-  const sortedExpenses = [...groupExpenses].sort((a, b) => (
+  // check if no groupExpenses
+  if (!groupExpenses) return <p>Group expenses null</p>;
+
+  const handleMonthChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedMonth(event.target.value);
+  };
+
+  const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedYear(event.target.value);
+  };
+
+  const handleClearFilters = () => {
+    setSelectedMonth(null);
+    setSelectedYear(null);
+  }
+
+  // filter expenses by month, year
+  const filterExpenses = (expenses: Expense[]) => {
+    return expenses.filter((expense) => {
+      const expenseDate = new Date(expense.date);
+      const expenseMonth = expenseDate.getMonth() + 1;
+      const expenseYear = expenseDate.getFullYear();
+
+      const monthMatches = selectedMonth ? parseInt(selectedMonth) === expenseMonth : true;
+      const yearMatches = selectedYear ? parseInt(selectedYear) === expenseYear : true;
+
+      return monthMatches && yearMatches;
+    })
+  };
+
+  const filteredExpenses = filterExpenses(groupExpenses);
+
+  // sort expenses from most recent to oldest
+  const sortedExpenses = filteredExpenses.sort((a, b) => (
     new Date(b.date).getTime() - new Date(a.date).getTime()
   ));
 
@@ -32,9 +66,26 @@ const ExpenseList: React.FC<ExpenseListProps> = ({ group, groupExpenses, onUpdat
       );
     }) : <p>No expenses in list</p>;
 
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
   return (
     <div className="expense-list-container">
       <h2>Expense List</h2>
+      <div className="filter-container">
+        <select value={selectedMonth || ""} onChange={handleMonthChange}>
+          <option value="">All Months</option>
+          {monthNames.map((month, index) => (
+            <option key={index} value={month}>{month}</option>
+          ))}
+        </select>
+        <select value={selectedYear || ""} onChange={handleYearChange}>
+          <option value="">All Years</option>
+          {Array.from(new Set(groupExpenses.map(expense => new Date(expense.date).getFullYear()))).map(year => (
+            <option key={year} value={year}>{year}</option>
+          ))}
+        </select>
+        <button className="clear-filter-button" onClick={handleClearFilters}>Clear Filters</button>
+      </div>
       <ul className='expense-list'>
         {expenseComponents}
       </ul>
